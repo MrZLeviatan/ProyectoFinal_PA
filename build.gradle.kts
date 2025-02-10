871031571607141b301827f6e4bead8c01563e70
Nicolas C.S <nikis281002@gmail.com>
2025-02-10 00:40:31 -0500

    Se crea la base del repositorio
    Se crea la base en la que se trabajara en el proyecto final

---
+++ build.gradle.kts
@@ -0,0 +1,39 @@
+plugins {
+    java
+    id("org.springframework.boot") version "3.4.1"
+    id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "co.edu.uniquindio"
+version = "1.0-SNAPSHOT"
+description = "Proyecto Final de Programación Avanzada. Atte: Mr Leviatan"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
+configurations {
+    compileOnly {
+        extendsFrom(configurations.annotationProcessor.get())
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+    compileOnly("org.projectlombok:lombok")
+    annotationProcessor("org.projectlombok:lombok")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
