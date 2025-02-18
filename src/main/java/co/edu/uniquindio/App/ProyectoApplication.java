cf894a647569b5002b62ea2d23be84d277af208e
Andrey3681 <117780962+Andrey3681@users.noreply.github.com>
2025-02-18 13:42:54 -0500

    agrege una anotaciòn @ComponentScan para decirle de donde va estar escuchando para enviar la info

--- src/main/java/co/edu/uniquindio/App/ProyectoApplication.java
+++ src/main/java/co/edu/uniquindio/App/ProyectoApplication.java
@@ -2,9 +2,11 @@ package co.edu.uniquindio.App;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 
 @SpringBootApplication
+@ComponentScan(basePackages = "co.edu.uniquindio.controller")
 public class ProyectoApplication {
 
     public static void main(String[] args) {
