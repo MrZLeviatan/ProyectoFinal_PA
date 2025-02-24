bfab286279f187f174393c881e38f6661cf02d4b
Andrey3681 <117780962+Andrey3681@users.noreply.github.com>
2025-02-24 11:01:27 -0500

    cambie el nombre del paquete por buenas practicas                                                      * todo los atributos que necesitan actualizarse                                                      */ticas

--- src/main/java/co/edu/uniquindio/proyecto/ProyectoApplication.java
+++ src/main/java/co/edu/uniquindio/proyecto/ProyectoApplication.java
@@ -1,4 +1,4 @@
-package co.edu.uniquindio.App;
+package co.edu.uniquindio.proyecto;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
