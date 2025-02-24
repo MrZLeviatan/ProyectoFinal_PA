1444c329ed92f5f09aee521838c35f094fc0b883
Andrey3681 <117780962+Andrey3681@users.noreply.github.com>
2025-02-24 11:05:15 -0500

    Dto creado para manejar la api de obtener usuarios por correo

---
+++ src/main/java/co/edu/uniquindio/dto/usuario/ObtenerUsuarioEmailDto.java
@@ -0,0 +1,12 @@
+package co.edu.uniquindio.dto.usuario;
+
+import co.edu.uniquindio.model.enums.EstadoUsuario;
+import co.edu.uniquindio.model.enums.Rol;
+
+public record ObtenerUsuarioEmailDto(
+        String correo
+
+) {
+
+}
+
