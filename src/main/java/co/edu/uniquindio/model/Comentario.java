0a2280e962af3517a4f61ed39a1c6f9650a612b0
Andrey3681 <117780962+Andrey3681@users.noreply.github.com>
2025-02-24 14:40:45 -0500

    dto para gestionar la eliminacion de una categoria

--- src/main/java/co/edu/uniquindio/model/Comentario.java
+++ src/main/java/co/edu/uniquindio/model/Comentario.java
@@ -11,8 +11,10 @@ import java.util.List;
 @NoArgsConstructor
 @NonNull
 public class Comentario {
-    private String contenido;
-    private String fechaComentario;
-    private String idUsuario;
+    private String contenido; // contenido
+    private String fechaComentario; //en que fecha se realizo el comentario
+    private String idUsuario; //quien realizo el comentario
+    private String id; //id unica del comentario
+    private String idReporte; //para saber a que reporte esta asociado el comentario
     private List<Comentario> comentarios= new ArrayList<>();
 }
