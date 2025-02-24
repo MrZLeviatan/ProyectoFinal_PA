2d46b041a9367fabe339816f84ea4ea1931a2cbc
Andrey3681 <117780962+Andrey3681@users.noreply.github.com>
2025-02-24 11:05:25 -0500

    Dto creado para manejar la api de obtener usuarios por id

---
+++ src/main/java/co/edu/uniquindio/dto/usuario/ObtenerUsuarioPorIdDto.java
@@ -0,0 +1,9 @@
+package co.edu.uniquindio.dto.usuario;
+
+import co.edu.uniquindio.model.enums.EstadoUsuario;
+import co.edu.uniquindio.model.enums.Rol;
+
+public record ObtenerUsuarioPorIdDto(
+       String id
+) {
+}
