6224b12b3259abf6b7a87309aefd84591e348cee
Andrey3681 <117780962+Andrey3681@users.noreply.github.com>
2025-02-23 22:49:54 -0500

    controlador de la calse ubucacion

---
+++ src/main/java/co/edu/uniquindio/controller/UbicacionController.java
@@ -0,0 +1,9 @@
+package co.edu.uniquindio.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/ubicacion")
+public class UbicacionController {
+}
