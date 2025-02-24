0bece7230e074e73e0d3964459aa7bb5930d5bcc
Andrey3681 <117780962+Andrey3681@users.noreply.github.com>
2025-02-23 22:50:07 -0500

    controlador de la clase usuario

---
+++ src/main/java/co/edu/uniquindio/controller/UsuarioController.java
@@ -0,0 +1,10 @@
+package co.edu.uniquindio.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/usuario")
+
+public class UsuarioController {
+}
