0d0d2b985aff665b0a12e45a3b9c169445bc28d2
Andrey3681 <117780962+Andrey3681@users.noreply.github.com>
2025-02-28 23:43:40 -0500

    Update ObtenerUsuarioPorIdDto.java
    eliminar importaciones inncesarias y comentario

--- src/main/java/co/edu/uniquindio/dto/usuario/ObtenerUsuarioPorIdDto.java
+++ src/main/java/co/edu/uniquindio/dto/usuario/ObtenerUsuarioPorIdDto.java
@@ -1,9 +1,5 @@
 package co.edu.uniquindio.dto.usuario;
-
-import co.edu.uniquindio.model.enums.EstadoUsuario;
-import co.edu.uniquindio.model.enums.Rol;
-
 public record ObtenerUsuarioPorIdDto(
-       String id
+       String id //id del usuario a buscar
 ) {
 }
