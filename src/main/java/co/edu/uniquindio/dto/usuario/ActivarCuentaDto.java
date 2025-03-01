f2bb891be35cb6d4126bd6010c348d6ea031d430
Andrey3681 <117780962+Andrey3681@users.noreply.github.com>
2025-02-28 23:44:41 -0500

    Update ActivarCuentaDto.java
    eliminacion de parte elemento innecesario del comentario

--- src/main/java/co/edu/uniquindio/dto/usuario/ActivarCuentaDto.java
+++ src/main/java/co/edu/uniquindio/dto/usuario/ActivarCuentaDto.java
@@ -2,7 +2,7 @@ package co.edu.uniquindio.dto.usuario;
 
 public record ActivarCuentaDto(
 
-        String email,// * correo perteneciente al usuario a activar
+        String email,//  correo perteneciente al usuario a activar
 
         String codigo //codigo ingresado por el cliente
 ) {
