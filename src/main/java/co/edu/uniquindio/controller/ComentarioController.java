3b974cb16737a7aefddd53ecfef45d6b6d61172e
Andrey Tombe <117780962+Andrey3681@users.noreply.github.com>
2025-03-28 21:43:05 -0500

    load

--- src/main/java/co/edu/uniquindio/controller/ComentarioController.java
+++
@@ -1,9 +0,0 @@
-package co.edu.uniquindio.controller;
-
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping("/comentario")
-public class ComentarioController {
-}
