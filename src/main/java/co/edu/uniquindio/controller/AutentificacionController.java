19e8707f4d3e33784fdfd3478d78b3c76dd2ed37
Andrey Tombe <117780962+Andrey3681@users.noreply.github.com>
2025-03-30 22:18:52 -0500

    Update AutentificacionController.java
    actualice el controlador solucionando unos errores que cometi

--- src/main/java/co/edu/uniquindio/controller/AutentificacionController.java
+++ src/main/java/co/edu/uniquindio/controller/AutentificacionController.java
@@ -4,51 +4,53 @@ import co.edu.uniquindio.dto.LoginDto;
 import co.edu.uniquindio.dto.MensajeDTO;
 import co.edu.uniquindio.dto.RestablecerPasswordDto;
 import co.edu.uniquindio.dto.usuario.ActivarCuentaDto;
+import co.edu.uniquindio.dto.usuario.RegistrarUsuarioDto;
 import co.edu.uniquindio.dto.usuario.UsuarioDTO;
 import co.edu.uniquindio.services.AutentificacionService;
 import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/Autentifcar")
+@RequestMapping("api/Autentifcar")
+@RequiredArgsConstructor
 public class AutentificacionController {
 
-    AutentificacionService autentificacionService;
-// como hacer que me diga si va ser admin o no
+    private final AutentificacionService autentificacionService;
 
-    @PostMapping("/login")
+    // Iniciar sesión
+    @PostMapping("/iniciarSesion")
     public ResponseEntity<MensajeDTO<String>> iniciarSesion(@Valid @RequestBody LoginDto loginDTO ) throws Exception{
-
-        //TODO Llamar al servicio de autenticacion para validar el usuario y contraseña
-
         autentificacionService.iniciarSesion(loginDTO);
-
-        return ResponseEntity.status(200).body( new MensajeDTO<>(false, "Inicio de sesion exitoso"));
+        return ResponseEntity.status(200).body(new MensajeDTO<>(false, "Inicio de sesión exitoso"));
     }
 
-    @PostMapping
-    public ResponseEntity<MensajeDTO<String>> crearUsuario(@Valid @RequestBody UsuarioDTO usuarioDTO ) throws Exception{
+    // Crear usuario
+    @PostMapping("/crearUsuario")
+    public ResponseEntity<MensajeDTO<String>> crearUsuario(@Valid @RequestBody RegistrarUsuarioDto usuarioDTO ) throws Exception{
         autentificacionService.crearUsuario(usuarioDTO);
-        return ResponseEntity.status(200).body( new MensajeDTO<>(true, "Usuario creado"));
-
+        return ResponseEntity.status(200).body(new MensajeDTO<>(true, "Usuario creado"));
     }
 
-    @PutMapping
+    // Solicitar restablecimiento de contraseña
+    @PutMapping("/solicitarRestablecer")
     public ResponseEntity<MensajeDTO<String>> solicitarRestablecer(@Valid @RequestBody String email ) throws Exception{
         autentificacionService.solicitarRestablecer(email);
-        return ResponseEntity.status(200).body( new MensajeDTO<>(true, "Restablecer"));
+        return ResponseEntity.status(200).body(new MensajeDTO<>(true, "Restablecer"));
     }
 
-    @PostMapping
+    // Restablecer contraseña
+    @PostMapping("/restablecerPassword")
     public ResponseEntity<MensajeDTO<String>> restablecerPassword(@Valid @RequestBody RestablecerPasswordDto restablecerPasswordDto ) throws Exception{
         autentificacionService.restablecerPassword(restablecerPasswordDto);
-        return ResponseEntity.status(200).body( new MensajeDTO<>(true, "Password restablecida"));
+        return ResponseEntity.status(200).body(new MensajeDTO<>(true, "Password restablecida"));
     }
 
-    @PostMapping
+    // Activar cuenta
+    @PostMapping("/activarCuenta")
     public ResponseEntity<MensajeDTO<String>> autentificarCuenta(@Valid @RequestBody ActivarCuentaDto activarCuentaDto ) throws Exception{
         autentificacionService.activarCuenta(activarCuentaDto);
-        return ResponseEntity.status(200).body( new MensajeDTO<>(true, "Cuenta activada"));
+        return ResponseEntity.status(200).body(new MensajeDTO<>(true, "Cuenta activada"));
     }
 }
