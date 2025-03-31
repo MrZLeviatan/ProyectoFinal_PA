412b9048c28eff94574c78893cba11f7582ee6ec
Andrey Tombe <117780962+Andrey3681@users.noreply.github.com>
2025-03-31 05:41:23 -0500

    Update Reporte.java
    agrege los getter y setter manualmente para evitar problemas con el mapper

--- src/main/java/co/edu/uniquindio/model/Reporte.java
+++ src/main/java/co/edu/uniquindio/model/Reporte.java
@@ -13,8 +13,7 @@ import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.List;
-@Getter
-@Setter
+
 @NoArgsConstructor
 @AllArgsConstructor
 @Document(collection = "reportes")
@@ -23,14 +22,111 @@ public class Reporte {
     @Id
     private ObjectId id;
     private String titulo;
-    private String idUsuario;
+    private ObjectId idUsuario; // hago referencia al usuario atravez de su id
     private Ubicacion ubicacion;
     private EstadoResulto estadoReporte;
     private Categoria categoria;
     @DBRef  private List<Comentario> comentarios;
     private EstadoReporte verificado; // admin
     private List<HistorialEstado> historial;
-    private String foto;
+    private List<String> fotos; //lista de rutas
     private int numeroImportancia;
     private EstadoSeveridad severidad;
+
+
+    public ObjectId getId() {
+        return id;
+    }
+
+    public void setId(ObjectId id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public ObjectId getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(ObjectId idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public Ubicacion getUbicacion() {
+        return ubicacion;
+    }
+
+    public void setUbicacion(Ubicacion ubicacion) {
+        this.ubicacion = ubicacion;
+    }
+
+    public EstadoResulto getEstadoReporte() {
+        return estadoReporte;
+    }
+
+    public void setEstadoReporte(EstadoResulto estadoReporte) {
+        this.estadoReporte = estadoReporte;
+    }
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
+
+    public List<Comentario> getComentarios() {
+        return comentarios;
+    }
+
+    public void setComentarios(List<Comentario> comentarios) {
+        this.comentarios = comentarios;
+    }
+
+    public EstadoReporte getVerificado() {
+        return verificado;
+    }
+
+    public void setVerificado(EstadoReporte verificado) {
+        this.verificado = verificado;
+    }
+
+    public List<HistorialEstado> getHistorial() {
+        return historial;
+    }
+
+    public void setHistorial(List<HistorialEstado> historial) {
+        this.historial = historial;
+    }
+
+    public List<String> getFotos() {
+        return fotos;
+    }
+
+    public void setFotos(List<String> fotos) {
+        this.fotos = fotos;
+    }
+
+    public int getNumeroImportancia() {
+        return numeroImportancia;
+    }
+
+    public void setNumeroImportancia(int numeroImportancia) {
+        this.numeroImportancia = numeroImportancia;
+    }
+
+    public EstadoSeveridad getSeveridad() {
+        return severidad;
+    }
+
+    public void setSeveridad(EstadoSeveridad severidad) {
+        this.severidad = severidad;
+    }
 }
