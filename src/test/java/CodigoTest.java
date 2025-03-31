60e6387d4c4ddc7392c2fe6db5779e1d1752b5de
Andrey Tombe <117780962+Andrey3681@users.noreply.github.com>
2025-03-30 22:08:48 -0500

    Test
    agrege la prueba para enviar un correo con la nueva implementacion

--- src/test/java/CodigoTest.java
+++ src/test/java/CodigoTest.java
@@ -1,11 +1,12 @@
-import co.edu.uniquindio.utils.EnviarCodigoActivacion;
-import co.edu.uniquindio.utils.service.Notificación;
+import co.edu.uniquindio.dto.EmailDto;
+import co.edu.uniquindio.services.EmailServicio;
+import co.edu.uniquindio.services.impl.EmailServicioImp;
 import org.junit.jupiter.api.Test;
 
 public class CodigoTest {
     @Test
-    void enviar(){
-        Notificación enviarCodigoActivacion = new EnviarCodigoActivacion("prueba","nikis281002@gmail.com","axmiadsladj");
-        enviarCodigoActivacion.EnviarNotificacion();
+    void enviarCorreo() throws Exception {
+        EmailServicio enviar= new EmailServicioImp();
+        enviar.enviarCorreo(new EmailDto("prueba", "prueba", "andrey3681.ay@gmail.com"));
     }
 }
