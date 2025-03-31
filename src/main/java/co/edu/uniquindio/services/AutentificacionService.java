845e1a7fa49bbe3036b080e1283b8b1f56593234
Andrey Tombe <117780962+Andrey3681@users.noreply.github.com>
2025-03-30 22:13:51 -0500

    Service(update)
    actualize el contrato para la autentificacion

--- src/main/java/co/edu/uniquindio/services/AutentificacionService.java
+++ src/main/java/co/edu/uniquindio/services/AutentificacionService.java
@@ -3,17 +3,18 @@ package co.edu.uniquindio.services;
 import co.edu.uniquindio.dto.LoginDto;
 import co.edu.uniquindio.dto.RestablecerPasswordDto;
 import co.edu.uniquindio.dto.usuario.ActivarCuentaDto;
-import co.edu.uniquindio.dto.usuario.UsuarioDTO;
+import co.edu.uniquindio.dto.usuario.RegistrarUsuarioDto;
+import co.edu.uniquindio.exeptions.UsuarioException;
 import jakarta.validation.Valid;
 
 public interface AutentificacionService {
     void iniciarSesion(LoginDto loginDTO) throws Exception;
 
-    void crearUsuario(UsuarioDTO usuarioDTO);
+    void crearUsuario(RegistrarUsuarioDto usuarioDTO) throws UsuarioException;
 
     void restablecerPassword(RestablecerPasswordDto restablecerPasswordDto);
 
-    void solicitarRestablecer(@Valid String email);
+    void solicitarRestablecer(@Valid String email) throws Exception;
 
     void activarCuenta(ActivarCuentaDto activarCuentaDto) throws Exception;
 }
