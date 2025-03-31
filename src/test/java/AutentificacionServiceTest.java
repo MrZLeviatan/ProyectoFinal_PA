c140c2fc68248dfa0543afcc588aa6279455a0eb
Andrey Tombe <117780962+Andrey3681@users.noreply.github.com>
2025-03-31 05:38:34 -0500

    Update AutentificacionServiceTest.java

--- src/test/java/AutentificacionServiceTest.java
+++ src/test/java/AutentificacionServiceTest.java
@@ -3,10 +3,16 @@ import co.edu.uniquindio.services.impl.AuntentificacionImplement;
 import org.junit.jupiter.api.Test;
 
 public class AutentificacionServiceTest {
+    AutentificacionService service= new AuntentificacionImplement();
     @Test
     void enviarCodigoSolicitudRestablecer() throws Exception {
-        AutentificacionService service= new AuntentificacionImplement();
+
         service.solicitarRestablecer("andrey3681.ay@gmail.com");
+    }
+
+    @Test
+    void enviarCodigoAutentificacion() throws Exception {
+
     }
 
 
