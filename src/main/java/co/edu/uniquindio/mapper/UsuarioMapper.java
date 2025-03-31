d1a1ef6f1e792b10d93e7f12911b710763b14c93
Andrey Tombe <117780962+Andrey3681@users.noreply.github.com>
2025-03-31 05:43:11 -0500

    Update UsuarioMapper.java
    acomode las funciones del mapper

--- src/main/java/co/edu/uniquindio/mapper/UsuarioMapper.java
+++ src/main/java/co/edu/uniquindio/mapper/UsuarioMapper.java
@@ -1,23 +1,39 @@
 package co.edu.uniquindio.mapper;
 
+import co.edu.uniquindio.dto.usuario.CodigoValidacionDTO;
 import co.edu.uniquindio.dto.usuario.EditarUsuarioDto;
 import co.edu.uniquindio.dto.usuario.RegistrarUsuarioDto;
 import co.edu.uniquindio.dto.usuario.UsuarioDTO;
+import co.edu.uniquindio.model.CodigoValidacion;
 import co.edu.uniquindio.model.Usuario;
+import org.bson.types.ObjectId;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", uses = {NotificacionMapper.class, ReporteMapper.class, ObjectIdMapper.class})
 public interface UsuarioMapper {
 
-//    @Mapping(target = "rol", constant = "USUARIO")
-//    @Mapping(target = "estadoUsuario", constant = "INACTIVO")
-//    Usuario toDocument(RegistrarUsuarioDto usuarioDTO);
-//
-//    RegistrarUsuarioDto toDTO(Usuario usuario);
-//
-//    void toDocument(EditarUsuarioDto editarUsuarioDTO, @MappingTarget Usuario usuario);
+
+
+
+
+    // mapeo del usuario para registrarlo
+    @Mapping(target = "rol", constant = "USUARIO")
+    @Mapping(target = "estadoUsuario", constant = "INACTIVO")
+    @Mapping(target = "fechaRegistro", expression = "java(java.time.LocalDateTime.now())")
+    @Mapping(target = "notificaciones", expression = "java(new java.util.ArrayList<>())")
+    @Mapping(target = "reportes", expression = "java(new java.util.ArrayList<>())")
+    @Mapping(target = "listaReportesFavorito", expression = "java(new java.util.ArrayList<>())") // inicializamos las listas
+    @Mapping(target = "codigoValidacion", source = "codigoValidacionDTO") //le decimos que tranforme el dto con un metodo anteriormente creado
+    @Mapping(target = "id", ignore = true) //ignoamos la id la crea mongo al guardarse
+    Usuario toDocument(RegistrarUsuarioDto usuarioDTO);
+
+
+
+    // usuario a usuario Dto
+     UsuarioDTO toUsuarioDTO(Usuario usuario);
+
 
 
 
