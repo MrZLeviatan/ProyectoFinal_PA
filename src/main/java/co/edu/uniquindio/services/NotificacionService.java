af5bb317679034d7bacd331fbf4658465b8f9dd9
Andrey Tombe <117780962+Andrey3681@users.noreply.github.com>
2025-04-07 17:12:56 -0500

    Create NotificacionService.java
    contrato para las notificaciones

---
+++ src/main/java/co/edu/uniquindio/services/NotificacionService.java
@@ -0,0 +1,14 @@
+package co.edu.uniquindio.services;
+
+import co.edu.uniquindio.dto.modeloDTO.NotificacionDTOM;
+
+import java.util.List;
+
+public interface NotificacionService {
+    void enviarNotificacion(NotificacionDTOM notificacionDTOM);
+    List<NotificacionDTOM> leerNotificaciones(String idUsuario);
+    NotificacionDTOM buscarNotificacion(String idNotificacion);
+    void EliminarNotificacion(String idNotificacion);
+
+
+}
