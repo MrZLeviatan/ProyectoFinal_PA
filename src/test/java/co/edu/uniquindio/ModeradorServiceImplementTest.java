3644e4f0c4fef0d5f1907d77eece2bdcb66badc6
Andrey Tombe <117780962+Andrey3681@users.noreply.github.com>
2025-04-06 19:51:28 -0500

    Create ModeradorServiceImplementTest.java
    donde se realizaran los test de los servicios del administrador

---
+++ src/test/java/co/edu/uniquindio/ModeradorServiceImplementTest.java
@@ -0,0 +1,14 @@
+package co.edu.uniquindio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ModeradorServiceImplementTest {
+
+    @Test
+    public void mostrarReporte() throws Exception {
+        
+
+    }
+}
