9ef641330f814cd3f25606e3a78be14df27a0409
William Carmona Díaz <william.carmonad@uqvirtual.edu.co>
2025-04-12 03:14:49 -0500

    Update NotificacionDTO.java

--- src/main/java/co/edu/uniquindio/services/impl/NotificacionDTO.java
+++ src/main/java/co/edu/uniquindio/services/impl/NotificacionDTO.java
@@ -1,5 +1,9 @@
 package co.edu.uniquindio.services.impl;
 
+/**
+ * DTO para la notificación.
+ * Esta clase se usa para representar una notificación que se enviará a un grupo de usuarios.
+ */
 public record NotificacionDTO(
         String titulo,
         String cuerpo,
