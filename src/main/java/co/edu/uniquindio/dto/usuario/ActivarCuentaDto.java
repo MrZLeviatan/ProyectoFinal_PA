87bb52b944cd1da1a45095a3b68523831b014976
William Carmona Díaz <william.carmonad@uqvirtual.edu.co>
2025-04-12 02:31:45 -0500

    Update ActivarCuentaDto.java

--- src/main/java/co/edu/uniquindio/dto/usuario/ActivarCuentaDto.java
+++ src/main/java/co/edu/uniquindio/dto/usuario/ActivarCuentaDto.java
@@ -2,10 +2,17 @@ package co.edu.uniquindio.dto.usuario;
 
 import jakarta.validation.constraints.NotBlank;
 
+
+/**
+ * DTO utilizado para activar una cuenta de usuario.
+ *
+ * @param id                Correo del usuario que desea activar la cuenta.
+ * @param codigoActivacion  Código de validación ingresado por el usuario.
+ */
 public record ActivarCuentaDto(
 
-        @NotBlank String id,    // Correo perteneciente al usuario a activar
-        @NotBlank CodigoValidacionDTO codigoActivacion // Código ingresado por el cliente
-) {
+        @NotBlank String id,
+        @NotBlank CodigoValidacionDTO codigoActivacion
 
+) {
 }
