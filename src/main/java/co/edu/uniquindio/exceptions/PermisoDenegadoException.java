33fcdbbd1435dd5d8570d492c83656541ccba2cb
William Carmona Díaz <william.carmonad@uqvirtual.edu.co>
2025-04-12 01:14:32 -0500

    Create PermisoDenegadoException.java

---
+++ src/main/java/co/edu/uniquindio/exceptions/PermisoDenegadoException.java
@@ -0,0 +1,7 @@
+package co.edu.uniquindio.exceptions;
+
+public class PermisoDenegadoException extends RuntimeException {
+  public PermisoDenegadoException(String message) {
+    super(message);
+  }
+}
