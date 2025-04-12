f0f317f00d7afbe22390d4a3dbeef96e03443f77
William Carmona Díaz <william.carmonad@uqvirtual.edu.co>
2025-04-12 02:00:39 -0500

    Update EditarCategoriaDto.java

--- src/main/java/co/edu/uniquindio/dto/moderador/EditarCategoriaDto.java
+++ src/main/java/co/edu/uniquindio/dto/moderador/EditarCategoriaDto.java
@@ -3,6 +3,11 @@ package co.edu.uniquindio.dto.moderador;
 import jakarta.validation.constraints.NotBlank;
 import org.hibernate.validator.constraints.Length;
 
+/**
+ * DTO utilizado para editar una categoría ya existente.
+ * Contiene el nombre y la descripción de la categoría.
+ * Ambos campos son obligatorios y tienen restricciones de longitud.
+ */
 public record EditarCategoriaDto(
 
         @NotBlank String id,
