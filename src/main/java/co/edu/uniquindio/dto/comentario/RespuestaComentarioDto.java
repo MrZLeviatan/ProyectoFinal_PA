f23b27118f60a20749e515741c251bee35077bf4
William Carmona Díaz <william.carmonad@uqvirtual.edu.co>
2025-04-12 01:55:02 -0500

    Update RespuestaComentarioDto.java

--- src/main/java/co/edu/uniquindio/dto/comentario/RespuestaComentarioDto.java
+++ src/main/java/co/edu/uniquindio/dto/comentario/RespuestaComentarioDto.java
@@ -3,6 +3,12 @@ package co.edu.uniquindio.dto.comentario;
 import jakarta.validation.constraints.NotBlank;
 import org.hibernate.validator.constraints.Length;
 
+/**
+ * DTO utilizado para registrar una respuesta a un comentario en el sistema.
+ * Contiene los datos necesarios para crear una respuesta, como el contenido,
+ * el comentario al que responde, el reporte al que está asociado, y el usuario
+ * que realiza la respuesta.
+ */
 public record RespuestaComentarioDto(
 
         @NotBlank String idComentario,
