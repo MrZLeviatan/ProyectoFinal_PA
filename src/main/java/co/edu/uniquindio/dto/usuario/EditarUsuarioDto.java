0ba5dfe975da9b86f96fcdca82ebd905c8e8cf26
William Carmona Díaz <william.carmonad@uqvirtual.edu.co>
2025-04-12 02:31:47 -0500

    Update EditarUsuarioDto.java

--- src/main/java/co/edu/uniquindio/dto/usuario/EditarUsuarioDto.java
+++ src/main/java/co/edu/uniquindio/dto/usuario/EditarUsuarioDto.java
@@ -5,10 +5,19 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import org.hibernate.validator.constraints.Length;
 
+/**
+ * DTO utilizado para editar la información de un usuario.
+ *
+ * @param id        ID del usuario a editar.
+ * @param nombre    Nuevo nombre del usuario (máximo 100 caracteres).
+ * @param ciudad    Nueva ciudad del usuario.
+ * @param direccion Nueva dirección del usuario (máximo 100 caracteres).
+ */
 public record EditarUsuarioDto(
         @NotBlank String id,
-        @NotBlank @Length(max = 100)String nombre,
+        @NotBlank @Length(max = 100) String nombre,
         @NotNull Ciudad ciudad,
         @NotBlank @Length(max = 100) String direccion
 ) {
 }
+
