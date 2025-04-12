7781040df5eb17a83cc0f0e82de303e9870a3f80
William Carmona Díaz <william.carmonad@uqvirtual.edu.co>
2025-04-12 02:31:36 -0500

    Update UbicacionDTO.java

--- src/main/java/co/edu/uniquindio/dto/reporte/UbicacionDTO.java
+++ src/main/java/co/edu/uniquindio/dto/reporte/UbicacionDTO.java
@@ -1,8 +1,16 @@
 package co.edu.uniquindio.dto.reporte;
 
+/**
+ * DTO que representa la ubicación geográfica de un reporte.
+ *
+ * @param latitud Coordenada de latitud.
+ * @param altitud Coordenada de altitud.
+ * @param radio   Radio de afectación en metros.
+ */
 public record UbicacionDTO(
         float latitud,
         float altitud,
         float radio
 ) {
 }
+
