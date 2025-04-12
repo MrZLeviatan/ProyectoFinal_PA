b475dcf75d622779004af6f244beba8ebf29e29e
William Carmona Díaz <william.carmonad@uqvirtual.edu.co>
2025-04-12 02:10:55 -0500

    Update PermisoDenegadoException.java

--- src/main/java/co/edu/uniquindio/exceptions/PermisoDenegadoException.java
+++ src/main/java/co/edu/uniquindio/exceptions/PermisoDenegadoException.java
@@ -1,5 +1,9 @@
 package co.edu.uniquindio.exceptions;
 
+/**
+ * Excepción personalizada lanzada cuando se deniega un permiso en el sistema.
+ * Se utiliza cuando un usuario no tiene los derechos necesarios para realizar una operación específica.
+ */
 public class PermisoDenegadoException extends RuntimeException {
   public PermisoDenegadoException(String message) {
     super(message);
