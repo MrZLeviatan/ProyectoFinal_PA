7a4349bfab684870544ac07e1f7dbdbba6514701
William Carmona Díaz <william.carmonad@uqvirtual.edu.co>
2025-04-12 01:14:29 -0500

    Create ElementoRepetidoException.java

---
+++ src/main/java/co/edu/uniquindio/exceptions/ElementoRepetidoException.java
@@ -0,0 +1,7 @@
+package co.edu.uniquindio.exceptions;
+
+public class ElementoRepetidoException extends RuntimeException {
+    public ElementoRepetidoException(String message) {
+        super(message);
+    }
+}
