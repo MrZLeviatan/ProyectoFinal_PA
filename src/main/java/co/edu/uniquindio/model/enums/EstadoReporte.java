397ddc799d49d7e00d5e44f8dd8ac749d0bba8ee
William Carmona Díaz <william.carmonad@uqvirtual.edu.co>
2025-04-12 02:43:17 -0500

    Update EstadoReporte.java

--- src/main/java/co/edu/uniquindio/model/enums/EstadoReporte.java
+++ src/main/java/co/edu/uniquindio/model/enums/EstadoReporte.java
@@ -1,5 +1,8 @@
 package co.edu.uniquindio.model.enums;
 
+/**
+ * Representa los diferentes estados que puede tener un Reporte dentro del sistema.
+ */
 public enum EstadoReporte {
     PENDIENTE,
     VERIFICADO,
