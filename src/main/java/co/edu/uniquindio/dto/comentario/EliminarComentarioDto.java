23bc9819be71277689d17c185abe91111b0753ab
William Carmona Díaz <william.carmonad@uqvirtual.edu.co>
2025-04-12 01:54:56 -0500

    Update EliminarComentarioDto.java

--- src/main/java/co/edu/uniquindio/dto/comentario/EliminarComentarioDto.java
+++ src/main/java/co/edu/uniquindio/dto/comentario/EliminarComentarioDto.java
@@ -1,9 +1,12 @@
 package co.edu.uniquindio.dto.comentario;
 
 import jakarta.validation.constraints.NotBlank;
-
+/**
+ * DTO utilizado para eliminar un comentario en el sistema.
+ * Contiene los datos necesarios para realizar la eliminación de un comentario
+ * en este caso el ID del comentario.
+ */
 public record EliminarComentarioDto(
-
         @NotBlank String idComentario
 ) {
 }
