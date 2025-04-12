901ae74cb9730aa6c9df435bf13981448a9925eb
William Carmona Díaz <william.carmonad@uqvirtual.edu.co>
2025-04-12 02:34:31 -0500

    Update RestablecerPasswordDto.java

--- src/main/java/co/edu/uniquindio/dto/RestablecerPasswordDto.java
+++ src/main/java/co/edu/uniquindio/dto/RestablecerPasswordDto.java
@@ -3,8 +3,14 @@ package co.edu.uniquindio.dto;
 import jakarta.validation.constraints.NotBlank;
 import org.hibernate.validator.constraints.Length;
 
+/**
+ * DTO utilizado para restablecer la contraseña de un usuario.
+ *
+ * @param email     Correo electrónico del usuario.
+ * @param password  Nueva contraseña del usuario (entre 7 y 20 caracteres).
+ */
 public record RestablecerPasswordDto(
         @NotBlank String email,
         @NotBlank @Length(min = 7, max = 20) String password
-        ) {
+) {
 }
