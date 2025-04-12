9124d490b994aca5593f82e3ea83fd0fc149251b
William Carmona Díaz <william.carmonad@uqvirtual.edu.co>
2025-04-12 02:43:20 -0500

    Update EstadoUsuario.java

--- src/main/java/co/edu/uniquindio/model/enums/EstadoUsuario.java
+++ src/main/java/co/edu/uniquindio/model/enums/EstadoUsuario.java
@@ -1,5 +1,8 @@
 package co.edu.uniquindio.model.enums;
 
+/**
+ * Representa los diferentes estados que puede tener una Usuario dentro del sistema.
+ */
 public enum EstadoUsuario {
     ACTIVO,
     INACTIVO,
