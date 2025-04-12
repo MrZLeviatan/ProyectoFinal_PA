cbf1d6488501607e06fbc6dedf9a235a3074055c
William Carmona Díaz <william.carmonad@uqvirtual.edu.co>
2025-04-12 02:00:35 -0500

    Update CrearCategoriaDto.java

--- src/main/java/co/edu/uniquindio/dto/moderador/CrearCategoriaDto.java
+++ src/main/java/co/edu/uniquindio/dto/moderador/CrearCategoriaDto.java
@@ -3,6 +3,11 @@ package co.edu.uniquindio.dto.moderador;
 import jakarta.validation.constraints.NotBlank;
 import org.hibernate.validator.constraints.Length;
 
+/**
+ * DTO utilizado para crear una nueva categoría.
+ * Contiene el nombre y la descripción de la categoría.
+ * Ambos campos son obligatorios y tienen restricciones de longitud.
+ */
 public record CrearCategoriaDto(
 
         @NotBlank @Length(max = 100) String nombre,
