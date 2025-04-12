25b62a0771d2055b268d5f2a91fc548c013758e2
William Carmona Díaz <william.carmonad@uqvirtual.edu.co>
2025-04-12 02:43:23 -0500

    Update EstadoSeveridad.java

--- src/main/java/co/edu/uniquindio/model/enums/EstadoSeveridad.java
+++ src/main/java/co/edu/uniquindio/model/enums/EstadoSeveridad.java
@@ -1,5 +1,8 @@
 package co.edu.uniquindio.model.enums;
 
+/**
+ * Representa los diferentes estados de Severidad que puede tener un reporte dentro del sistema.
+ */
 public enum EstadoSeveridad {
     BAJA,
     MEDIA,
