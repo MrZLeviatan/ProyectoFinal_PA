9e55d66e9fef394113301d4bda73c1d1c54e6a41
William Carmona Díaz <william.carmonad@uqvirtual.edu.co>
2025-04-12 02:43:26 -0500

    Update Rol.java

--- src/main/java/co/edu/uniquindio/model/enums/Rol.java
+++ src/main/java/co/edu/uniquindio/model/enums/Rol.java
@@ -1,5 +1,9 @@
 package co.edu.uniquindio.model.enums;
 
+/**
+ * Representa los diferentes roles que un usuario puede tener dentro del sistema.
+ * Esto determina el acceso y las funcionalidades que el usuario puede usar.
+ */
 public enum Rol {
     USUARIO,
     MODERADOR
