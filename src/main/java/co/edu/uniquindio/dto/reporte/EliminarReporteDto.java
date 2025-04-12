1bb56ac656ca233596c90359c54cf6c557bd343f
William Carmona Díaz <william.carmonad@uqvirtual.edu.co>
2025-04-12 02:31:20 -0500

    Update EliminarReporteDto.java

--- src/main/java/co/edu/uniquindio/dto/reporte/EliminarReporteDto.java
+++ src/main/java/co/edu/uniquindio/dto/reporte/EliminarReporteDto.java
@@ -2,8 +2,14 @@ package co.edu.uniquindio.dto.reporte;
 
 import jakarta.validation.constraints.NotBlank;
 
+/**
+ * DTO utilizado para eliminar un reporte del sistema.
+ * Requiere la validación de la contraseña del usuario.
+ *
+ * @param idReporte ID del reporte que se desea eliminar.
+ * @param password Contraseña del usuario para confirmar la acción.
+ */
 public record EliminarReporteDto(
-
         @NotBlank String idReporte,
         @NotBlank String password
 ) {
