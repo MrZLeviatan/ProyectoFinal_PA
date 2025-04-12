f05ab1dc1b07687ad9d6629163861b2247f56490
William Carmona Díaz <william.carmonad@uqvirtual.edu.co>
2025-04-12 02:34:28 -0500

    Update MensajeDTO.java

--- src/main/java/co/edu/uniquindio/dto/MensajeDTO.java
+++ src/main/java/co/edu/uniquindio/dto/MensajeDTO.java
@@ -1,4 +1,11 @@
 package co.edu.uniquindio.dto;
 
+/**
+ * DTO genérico que representa un mensaje con posible error.
+ *
+ * @param error   Indica si ocurrió un error (true = error).
+ * @param mensaje Contenido o información adicional del mensaje.
+ * @param <T>     Tipo genérico del contenido del mensaje.
+ */
 public record MensajeDTO<T>(boolean error, T mensaje) {
 }
