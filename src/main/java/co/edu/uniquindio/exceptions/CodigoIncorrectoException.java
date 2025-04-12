dfe1724d18a2652808e23dbb6ca7eeb7769d55ce
William Carmona Díaz <william.carmonad@uqvirtual.edu.co>
2025-04-12 01:14:13 -0500

    Create CodigoIncorrectoException.java

---
+++ src/main/java/co/edu/uniquindio/exceptions/CodigoIncorrectoException.java
@@ -0,0 +1,7 @@
+package co.edu.uniquindio.exceptions;
+
+public class CodigoIncorrectoException extends RuntimeException {
+    public CodigoIncorrectoException(String message) {
+        super(message);
+    }
+}
