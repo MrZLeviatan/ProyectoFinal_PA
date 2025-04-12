4320f5d044b2f77744509337aadc1cc25f8af570
William Carmona Díaz <william.carmonad@uqvirtual.edu.co>
2025-04-12 01:14:04 -0500

    Create CodigoExpiradoException.java

---
+++ src/main/java/co/edu/uniquindio/exceptions/CodigoExpiradoException.java
@@ -0,0 +1,7 @@
+package co.edu.uniquindio.exceptions;
+
+public class CodigoExpiradoException extends RuntimeException {
+    public CodigoExpiradoException(String message) {
+        super(message);
+    }
+}
