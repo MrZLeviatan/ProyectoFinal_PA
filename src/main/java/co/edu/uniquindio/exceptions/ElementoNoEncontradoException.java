88283441a9feef377636465f780d7acc19bda3da
William Carmona Díaz <william.carmonad@uqvirtual.edu.co>
2025-04-12 02:10:50 -0500

    Update ElementoNoEncontradoException.java

--- src/main/java/co/edu/uniquindio/exceptions/ElementoNoEncontradoException.java
+++ src/main/java/co/edu/uniquindio/exceptions/ElementoNoEncontradoException.java
@@ -1,5 +1,9 @@
 package co.edu.uniquindio.exceptions;
 
+/**
+ * Excepción personalizada lanzada cuando un elemento no se encuentra en el sistema.
+ * Se utiliza cuando se intenta acceder o realizar una operación con un elemento que no existe en la base de datos.
+ */
 public class ElementoNoEncontradoException extends RuntimeException {
     public ElementoNoEncontradoException(String message) {
         super(message);
