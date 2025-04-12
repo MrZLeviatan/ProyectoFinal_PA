de6bad1873a8604460aa08189bc74f43f85f9e43
William Carmona Díaz <william.carmonad@uqvirtual.edu.co>
2025-04-11 20:33:03 -0500

    Update AutentificacionService.java

--- src/main/java/co/edu/uniquindio/services/AutentificacionService.java
+++ src/main/java/co/edu/uniquindio/services/AutentificacionService.java
@@ -2,13 +2,14 @@ package co.edu.uniquindio.services;
 
 import co.edu.uniquindio.dto.LoginDto;
 import co.edu.uniquindio.dto.RestablecerPasswordDto;
+import co.edu.uniquindio.dto.TokenDTO;
 import co.edu.uniquindio.dto.usuario.ActivarCuentaDto;
 import co.edu.uniquindio.dto.usuario.RegistrarUsuarioDto;
 import co.edu.uniquindio.exeptions.UsuarioException;
 import jakarta.validation.Valid;
 
 public interface AutentificacionService {
-    void iniciarSesion(LoginDto loginDTO) throws Exception;
+    TokenDTO iniciarSesion(LoginDto loginDTO) throws Exception;
 
     void crearUsuario(RegistrarUsuarioDto usuarioDTO) throws UsuarioException;
 
