39fc582578ca937d1f9685646fa7ca167b68166a
William Carmona Díaz <william.carmonad@uqvirtual.edu.co>
2025-04-12 01:14:41 -0500

    Create UsuarioNoActivadoException.java

---
+++ src/main/java/co/edu/uniquindio/exceptions/UsuarioNoActivadoException.java
@@ -0,0 +1,7 @@
+package co.edu.uniquindio.exceptions;
+
+public class UsuarioNoActivadoException extends RuntimeException {
+    public UsuarioNoActivadoException(String message) {
+        super(message);
+    }
+}
