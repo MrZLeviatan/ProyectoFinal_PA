5aebbab9053353be79c6dc8f87ff640747bb1b87
William Carmona Díaz <william.carmonad@uqvirtual.edu.co>
2025-04-12 03:14:20 -0500

    Update AutentificacionService.java

--- src/main/java/co/edu/uniquindio/services/AutentificacionService.java
+++ src/main/java/co/edu/uniquindio/services/AutentificacionService.java
@@ -6,9 +6,7 @@ import co.edu.uniquindio.exceptions.CredencialesInvalidasException;
 import co.edu.uniquindio.exceptions.ElementoNoEncontradoException;
 import co.edu.uniquindio.exceptions.UsuarioNoActivadoException;
 
-
 public interface AutentificacionService {
     TokenDTO iniciarSesion(LoginDto loginDTO) throws ElementoNoEncontradoException, UsuarioNoActivadoException,
             CredencialesInvalidasException;
-
 }
