d262a4a56f6d77fd4f59b5fef1a58e365e4366b5
William Carmona Díaz <william.carmonad@uqvirtual.edu.co>
2025-04-12 03:13:52 -0500

    Update NotificacionRepo.java

--- src/main/java/co/edu/uniquindio/repositorios/NotificacionRepo.java
+++ src/main/java/co/edu/uniquindio/repositorios/NotificacionRepo.java
@@ -5,6 +5,10 @@ import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+/**
+ * Interfaz para la gestión de notificaciones en la base de datos MongoDB.
+ * Extiende MongoRepository para operaciones CRUD básicas.
+ */
 @Repository
 public interface NotificacionRepo extends MongoRepository<Notificacion, ObjectId> {
 
