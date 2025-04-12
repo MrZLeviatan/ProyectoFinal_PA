bcccc0e57c7fb86f5f09f5d28f72851975d9c955
William Carmona Díaz <william.carmonad@uqvirtual.edu.co>
2025-04-12 02:31:26 -0500

    Update MarcarReporteDto.java

--- src/main/java/co/edu/uniquindio/dto/reporte/MarcarReporteDto.java
+++ src/main/java/co/edu/uniquindio/dto/reporte/MarcarReporteDto.java
@@ -2,9 +2,16 @@ package co.edu.uniquindio.dto.reporte;
 
 import jakarta.validation.constraints.NotBlank;
 
+/**
+ * DTO utilizado para marcar un reporte como importante por parte de un usuario.
+ *
+ * @param idUsuario ID del usuario que marca el reporte como importante.
+ * @param idReporte ID del reporte que se desea marcar.
+ */
 public record MarcarReporteDto(
 
-        @NotBlank String idUsuario, // Id del usuario que marca el reporte como importante
+        @NotBlank String idUsuario,
         @NotBlank String idReporte
+
 ) {
 }
