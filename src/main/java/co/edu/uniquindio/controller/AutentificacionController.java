8da14bbd87a8e38199600dd82fed8db1c2457866
William Carmona Díaz <william.carmonad@uqvirtual.edu.co>
2025-04-11 20:32:58 -0500

    Update AutentificacionController.java

--- src/main/java/co/edu/uniquindio/controller/AutentificacionController.java
+++ src/main/java/co/edu/uniquindio/controller/AutentificacionController.java
@@ -2,11 +2,6 @@ package co.edu.uniquindio.controller;
 
 import co.edu.uniquindio.dto.LoginDto;
 import co.edu.uniquindio.dto.MensajeDTO;
-import co.edu.uniquindio.dto.RestablecerPasswordDto;
-import co.edu.uniquindio.dto.TokenDTO;
-import co.edu.uniquindio.dto.usuario.ActivarCuentaDto;
-import co.edu.uniquindio.dto.usuario.RegistrarUsuarioDto;
-import co.edu.uniquindio.dto.usuario.UsuarioDTO;
 import co.edu.uniquindio.services.AutentificacionService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
