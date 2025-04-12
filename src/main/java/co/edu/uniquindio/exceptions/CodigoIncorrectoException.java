d32255ab70caf3f3eaacedeb99e7b8fbc1a487f5
William Carmona Díaz <william.carmonad@uqvirtual.edu.co>
2025-04-12 02:10:45 -0500

    Update CodigoIncorrectoException.java

--- src/main/java/co/edu/uniquindio/exceptions/CodigoIncorrectoException.java
+++ src/main/java/co/edu/uniquindio/exceptions/CodigoIncorrectoException.java
@@ -1,5 +1,10 @@
 package co.edu.uniquindio.exceptions;
 
+/**
+ * Excepción personalizada lanzada cuando un código es incorrecto.
+ * Esta excepción se utiliza cuando el código ingresado no es válido o no coincide con los registros
+ * en el sistema, por ejemplo, un código de verificación erróneo.
+ */
 public class CodigoIncorrectoException extends RuntimeException {
     public CodigoIncorrectoException(String message) {
         super(message);
