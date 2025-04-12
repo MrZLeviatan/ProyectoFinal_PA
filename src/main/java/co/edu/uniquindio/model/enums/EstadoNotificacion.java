3bd0f72bc6637a530cdcf9f5be7b1d4d9d89f9e4
William Carmona Díaz <william.carmonad@uqvirtual.edu.co>
2025-04-12 02:43:13 -0500

    Update EstadoNotificacion.java

--- src/main/java/co/edu/uniquindio/model/enums/EstadoNotificacion.java
+++ src/main/java/co/edu/uniquindio/model/enums/EstadoNotificacion.java
@@ -1,5 +1,8 @@
 package co.edu.uniquindio.model.enums;
 
+/**
+ * Representa los diferentes estados que puede tener una Notificacion dentro del sistema.
+ */
 public enum EstadoNotificacion {
     LEIDO,
     SIN_LEER
