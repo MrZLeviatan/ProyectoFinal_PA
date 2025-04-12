f9525331a102b3b9f801dba670964ed7fe86b67b
William Carmona Díaz <william.carmonad@uqvirtual.edu.co>
2025-04-12 02:10:48 -0500

    Update ElementoRepetidoException.java

--- src/main/java/co/edu/uniquindio/exceptions/ElementoRepetidoException.java
+++ src/main/java/co/edu/uniquindio/exceptions/ElementoRepetidoException.java
@@ -1,5 +1,9 @@
 package co.edu.uniquindio.exceptions;
 
+/**
+ * Excepción personalizada lanzada cuando un elemento ya existe en el sistema.
+ * Se utiliza cuando se intenta agregar un elemento que ya está presente, como cuando se intenta crear un registro duplicado.
+ */
 public class ElementoRepetidoException extends RuntimeException {
     public ElementoRepetidoException(String message) {
         super(message);
