97c977a642d8ccb859815cf3580d650be0dc7609
William Carmona Díaz <william.carmonad@uqvirtual.edu.co>
2025-04-12 02:11:00 -0500

    Update UsuarioNoActivadoException.java

--- src/main/java/co/edu/uniquindio/exceptions/UsuarioNoActivadoException.java
+++ src/main/java/co/edu/uniquindio/exceptions/UsuarioNoActivadoException.java
@@ -1,5 +1,9 @@
 package co.edu.uniquindio.exceptions;
 
+/**
+ * Excepción personalizada lanzada cuando un usuario intenta realizar una acción sin haber activado su cuenta.
+ * Se utiliza cuando un usuario no ha completado el proceso de activación de cuenta y no puede acceder al sistema.
+ */
 public class UsuarioNoActivadoException extends RuntimeException {
     public UsuarioNoActivadoException(String message) {
         super(message);
