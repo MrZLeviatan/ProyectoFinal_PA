491bcbe499049d4c6b20f6b467b17461247385e0
William Carmona Díaz <william.carmonad@uqvirtual.edu.co>
2025-04-12 03:13:59 -0500

    Update UsuarioRepo.java

--- src/main/java/co/edu/uniquindio/repositorios/UsuarioRepo.java
+++ src/main/java/co/edu/uniquindio/repositorios/UsuarioRepo.java
@@ -11,14 +11,17 @@ import  org.springframework.data.domain.Pageable;
 import java.util.List;
 import java.util.Optional;
 
+/**
+ * Interfaz para la gestión de usuarios en la base de datos MongoDB.
+ * Extiende MongoRepository para operaciones CRUD básicas.
+ * Incluye un método adicional para buscar usuarios por su email y un método
+ * para obtener usuarios por nombre y ciudad, con soporte de paginación.
+ */
 @Repository
 public interface UsuarioRepo extends MongoRepository<Usuario,ObjectId> {
 
     Optional<Usuario> findByEmail(String email);
 
-
     @Query("{'nombre':?0 , 'ciudad' :?1}")
     List<Usuario> findByNombreYCiudad(String nombre, Ciudad ciudad, Pageable pageable);
-
-
 }
