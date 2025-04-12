4f7574abf16225747c405b39310e11b5713aae9a
William Carmona Díaz <william.carmonad@uqvirtual.edu.co>
2025-04-12 01:17:35 -0500

    Update UsuarioService.java

--- src/main/java/co/edu/uniquindio/services/UsuarioService.java
+++ src/main/java/co/edu/uniquindio/services/UsuarioService.java
@@ -2,13 +2,11 @@ package co.edu.uniquindio.services;
 
 import co.edu.uniquindio.dto.EliminarCuentaDto;
 import co.edu.uniquindio.dto.RestablecerPasswordDto;
-import co.edu.uniquindio.dto.TokenDTO;
 import co.edu.uniquindio.dto.usuario.EditarUsuarioDto;
 import co.edu.uniquindio.dto.usuario.UsuarioDTO;
 import co.edu.uniquindio.dto.usuario.*;
-import co.edu.uniquindio.services.impl.LoginDTO;
 import jakarta.validation.Valid;
-import co.edu.uniquindio.exeptions.ElementoRepetidoException;
+import co.edu.uniquindio.exceptions.ElementoRepetidoException;
 
 // Interfaz que define el contrato para las operaciones relacionadas con usuarios.
 public interface UsuarioService {
