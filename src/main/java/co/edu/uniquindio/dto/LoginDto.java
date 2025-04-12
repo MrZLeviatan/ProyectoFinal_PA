5017906967eda977014c760a966e0307f85a11d0
William Carmona Díaz <william.carmonad@uqvirtual.edu.co>
2025-04-12 02:34:26 -0500

    Update LoginDto.java

--- src/main/java/co/edu/uniquindio/dto/LoginDto.java
+++ src/main/java/co/edu/uniquindio/dto/LoginDto.java
@@ -4,8 +4,15 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import org.hibernate.validator.constraints.Length;
 
+/**
+ * DTO utilizado para realizar el inicio de sesión en el sistema.
+ *
+ * @param email     Correo electrónico del usuario.
+ * @param password  Contraseña del usuario (mínimo 7 caracteres).
+ */
 public record LoginDto(
         @NotBlank @Email String email,
-        @NotBlank @Length(min=7) String password
+        @NotBlank @Length(min = 7) String password
 ) {
 }
+
