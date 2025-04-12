ec19efc996dbc265d89bd964b1b759d1b9429e8b
William Carmona Díaz <william.carmonad@uqvirtual.edu.co>
2025-04-12 02:31:41 -0500

    Update RegistrarUsuarioDto.java

--- src/main/java/co/edu/uniquindio/dto/usuario/RegistrarUsuarioDto.java
+++ src/main/java/co/edu/uniquindio/dto/usuario/RegistrarUsuarioDto.java
@@ -6,13 +6,25 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import org.hibernate.validator.constraints.Length;
 
+/**
+ * DTO utilizado para registrar un nuevo usuario en el sistema.
+ *
+ * @param nombre             Nombre del usuario (máximo 100 caracteres).
+ * @param direccion          Dirección del usuario (máximo 100 caracteres).
+ * @param ciudad             Ciudad de residencia.
+ * @param email              Correo electrónico del usuario (máximo 50 caracteres).
+ * @param password           Contraseña segura (entre 7 y 20 caracteres).
+ * @param codigoValidacionDTO Código generado para validar el registro del usuario.
+ */
 public record RegistrarUsuarioDto(
 
         @NotBlank @Length(max = 100) String nombre,
-        @NotBlank @Length(max = 100)String direccion,
+        @NotBlank @Length(max = 100) String direccion,
         @NotNull Ciudad ciudad,
         @NotBlank @Length(max = 50) @Email String email,
         @NotBlank @Length(min = 7, max = 20) String password,
         @NotNull CodigoValidacionDTO codigoValidacionDTO
+
 ) {
 }
+
